d809f1d6dfc3905ffdd01619912b62f3fdc4c750
pkiop <pkiopb@gmail.com>
2022-07-11 20:09:27 +0900

    Feat:

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className='App'>
+      <img
+        alt='s3'
+        src='https://high-school-lecture.s3.ap-northeast-2.amazonaws.com/Picture1.png'
+      />
+      <div>S3 사진입니다. </div>
+    </div>
+  );
+}
+
+export default App;
